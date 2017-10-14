a85b4a7af8988169a3721018d2530182dd6e21af
Koppal <T12464@thomsonuk.dnsroot.biz>
2017-10-14 19:41:31 +0100

    comprice and underscore library added

--- static/src/comprice.js
+++ static/src/comprice.js
@@ -1,35 +1,50 @@
 (function () {
     
     $.ajax({
-        url: 'src/data.json'
+        url: 'http://10.33.178.124:8006/Comprice/rest/Comprice/price?username=raghu@gmail.com'
     }).done(function(data){
-        var count = 0;
-        for(var product in data) {
-            ++count;
+        var resData = JSON.parse(data),
+            alerts = resData.alerts, 
+            products = [], 
+            count = 0;
+        for(var i = 0; i < alerts.length; i++) {
+            var price = alerts[i].urls;
+            var obj = {
+                'columns': []
+            };
+            for(var j = 0; j < price.length; j++) {
+                obj.columns[i] = [];
+                obj.columns[i].push(price[j].name);
+                obj.columns[i].push(..._.pluck(price[j].pricehistory, 'price'));
+            }
+            products.push(obj);
+
             $('<li class="w3-padding-16">\
-                    <a href="#">\
-                    <span class="w3-xlarge">'+product+'</span></a>\
-                    <div class="content" style="padding: 20px;">\
-                        <div id="chart'+count+'">'+product+'</div>\
-                    </div>\
-                </li>')
+            <a href="#">\
+            <span class="w3-xlarge">'+alerts[i].name+'</span></a>\
+                <div class="content" style="padding: 20px;">\
+                    <div id="chart'+i+'">'+alerts[i].name+'</div>\
+                </div>\
+            </li>')
             .appendTo("ul.accordion" );
 
-            generateGraph(count, product, data);
+            generateGraph(i, products[i]);
         }
     });
 
-    function generateGraph(count, product, data) {
+    function generateGraph(count, product) {
         c3.generate({
             bindto: "#chart"+count,
-            data: data[product]
+            data: product
         });
     }
 
     $( document ).ready(function() {
-        $('ul.accordion li').on('click', function(e){
-            e.preventDefault();
-            $(this).find('.content').toggle();
-        });
+        setTimeout(function(){
+            $('ul.accordion li').on('click', function(e){
+                e.preventDefault();
+                $(this).find('.content').toggle();
+            });
+        },1000);
     });
 })();
